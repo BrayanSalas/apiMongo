15791f2ba154d35fa147dc1f6c50c5cbf87490f3
BrayanSalas <brayansalasch@hotmail.com>
2018-08-27 16:32:40 -0500

    'modifyFunctions'

--- controllers/lyrics.controller.js
+++ controllers/lyrics.controller.js
@@ -12,6 +12,13 @@ module.exports = {
         res.status(200).json({response, success:true})
     },
 
+    modifyLyrics: async (req, res) => {
+        const {id} = req.params
+        const modify = req.body
+        const response = await Lyrics.findByIdAndUpdate(id, modify)
+        res.status(200).json({response, success: true})
+    },
+
     deleteLyrics: async (req, res) => {
         const {id} = req.params
         const response = await Lyrics.findByIdAndDelete(id)
--- routes/lyrics.route.js
+++ routes/lyrics.route.js
@@ -1,9 +1,10 @@
 import routerInit from 'express-promise-router'
-import { index, newLyrics, deleteLyrics } from '../controllers/lyrics.controller'
+import { index, newLyrics, deleteLyrics, modifyLyrics } from '../controllers/lyrics.controller'
 const router = routerInit()
 
 router.get('/', index)
 router.post('/', newLyrics)
+router.put('/:id', modifyLyrics)
 router.delete('/:id', deleteLyrics)
 
 module.exports = router
